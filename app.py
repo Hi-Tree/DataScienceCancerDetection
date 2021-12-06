0ad12f133d4fd2b7969076f777d50e92f6367591
NourSleiman <32310672+NourSleiman@users.noreply.github.com>
2021-12-06 14:36:47 -0500

    upload model and other minor changes

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 import flask
 import os
+import pickle
 import pandas as pd
+from skimage import io
+from skimage import transform
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.optimizers import Adagrad
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from imutils import paths
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
+import sys
 
 app = flask.Flask(__name__, template_folder='templates')
+print(os.path.exists("models/finalized_mod.pkl")) # this prints out true
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -13,6 +28,27 @@ def main():
     if flask.request.method == 'POST':
         # Get the input from the user.
         user_input_text = flask.request.form['user_input_image']
+
+        BASE_PATH = "static/datasets"
+        TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+        BS = 32
+        H = pickle.load(open("models/finalized_mod.pkl","rb"))
+        totalTest = len(list(paths.list_images(TEST_PATH)))
+        valAug = ImageDataGenerator(rescale=1 / 255.0)
+
+        testGen = valAug.flow_from_directory(
+	    TEST_PATH,
+	    class_mode="categorical",
+	    target_size=(48, 48),
+	    color_mode="rgb",
+	    shuffle=False,
+	    batch_size=BS)
+
+        testGen.reset()
+        predIdxs = image_model.predict(x=testGen, steps=(totalTest // BS) + 1)
+        predIdxs = np.argmax(predIdxs, axis=1)
+        print(classification_report(testGen.classes, predIdxs,
+	    target_names=testGen.class_indices.keys()))
 
     
     return flask.render_template('index.html', 
