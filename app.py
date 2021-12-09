01d50d0b490153706d55d73bbf384a8309ee698d
NourSleiman <32310672+NourSleiman@users.noreply.github.com>
2021-12-09 13:34:30 -0500

    added graphs

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ import argparse
 import sys
 
 app = flask.Flask(__name__, template_folder='templates')
-print(os.path.exists("models/finalized_mod.pkl")) # this prints out true
+
+with open("finalized_model.sav", "rb") as p_f:
+	H = pickle.load(p_f)
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -27,33 +29,28 @@ def main():
     
     if flask.request.method == 'POST':
         # Get the input from the user.
-        user_input_text = flask.request.form['user_input_image']
+        file = flask.request.files['user_input_image']
 
-        BASE_PATH = "static/datasets"
-        TEST_PATH = os.path.sep.join([BASE_PATH, "testing"])
+        TEST_PATH = "Datasets/testing"
         BS = 32
-        H = pickle.load(open("models/finalized_mod.pkl","rb"))
         totalTest = len(list(paths.list_images(TEST_PATH)))
         valAug = ImageDataGenerator(rescale=1 / 255.0)
 
         testGen = valAug.flow_from_directory(
-	    TEST_PATH,
-	    class_mode="categorical",
-	    target_size=(48, 48),
-	    color_mode="rgb",
-	    shuffle=False,
-	    batch_size=BS)
+	        TEST_PATH,
+	        class_mode="categorical",
+	        target_size=(48, 48),
+	        color_mode="rgb",
+	        shuffle=False,
+	        batch_size=BS)
 
         testGen.reset()
-        predIdxs = image_model.predict(x=testGen, steps=(totalTest // BS) + 1)
+        predIdxs = H.predict(x=testGen, steps=(totalTest // BS) + 1)
         predIdxs = np.argmax(predIdxs, axis=1)
         print(classification_report(testGen.classes, predIdxs,
 	    target_names=testGen.class_indices.keys()))
 
-    
-    return flask.render_template('index.html', 
-            input_text=user_input_image,
-    )
+        return (flask.render_template('index.html'))
 
 if __name__ == '__main__':
     app.run(debug=True)
