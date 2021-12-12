e29c9f23592c8e15af83c9be9fece75a2c3550c9
Meri Khurshudyan <71793253+Hi-Tree@users.noreply.github.com>
2021-12-12 13:52:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import flask
-import os
 import pickle
-import pandas as pd
-from skimage import io
-from skimage import transform
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.optimizers import Adagrad
-from tensorflow.keras.utils import to_categorical
+
 from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
 from imutils import paths
-import matplotlib.pyplot as plt
+import os
 import numpy as np
-import argparse
 import sys
+from werkzeug.utils import secure_filename
+
+
+
 
 app = flask.Flask(__name__, template_folder='templates')
 
-with open("finalized_model.sav", "rb") as p_f:
+with open("models/finalized_model.sav", "rb") as p_f:
 	H = pickle.load(p_f)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -30,27 +26,32 @@ def main():
     if flask.request.method == 'POST':
         # Get the input from the user.
         file = flask.request.files['user_input_image']
-
-        TEST_PATH = "Datasets/testing"
-        BS = 32
+        
+        name = secure_filename(file.filename)
+        print(name)
+        file.save('Dataset/0/'+name)
+        os.chmod('Dataset/0/'+name, 0o0777)
+        TEST_PATH = 'Dataset'
+        BS = 1
+       
         totalTest = len(list(paths.list_images(TEST_PATH)))
         valAug = ImageDataGenerator(rescale=1 / 255.0)
-
         testGen = valAug.flow_from_directory(
-	        TEST_PATH,
-	        class_mode="categorical",
-	        target_size=(48, 48),
-	        color_mode="rgb",
-	        shuffle=False,
-	        batch_size=BS)
+            TEST_PATH,
+            class_mode="categorical",
+            target_size=(48, 48),
+            color_mode="rgb",
+            shuffle=False,
+            batch_size=BS)
 
         testGen.reset()
         predIdxs = H.predict(x=testGen, steps=(totalTest // BS) + 1)
         predIdxs = np.argmax(predIdxs, axis=1)
-        print(classification_report(testGen.classes, predIdxs,
-	    target_names=testGen.class_indices.keys()))
-
-        return (flask.render_template('index.html'))
+        p = "Cancerous"
+        pe = round(100 - H.predict(testGen)[:,1][0] * 100, 2)
+        if predIdxs[0] == 0:
+            p = "Not Cancerous"
+        return flask.render_template('index.html', prediction=p, percent = pe)
 
 @app.route('/graphs/')
 def graphs():
